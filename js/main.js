fdc89b95b7fad3ac169f976557c2579266380557
ivan19940106 <tanoshii06@gmail.com>
2021-06-14 17:49:29 +0800

    rewrite with Vue components

--- js/main.js
+++ js/main.js
@@ -3,32 +3,32 @@ $(document).ready(function(){
     var thisYear = today.getFullYear();
     $('.copyright-year').text(thisYear);
 
-    $('.card-grid').click(function(e){
-        e.preventDefault();
-        if(window.location.search == '?ss=on'){
-            window.location.href = 'card_content.html?ss=on';
-        } else {
-            window.location.href = 'card_content.html';
-        }
-    });
+    // $('.card-grid').click(function(e){
+    //     e.preventDefault();
+    //     if(window.location.search == '?ss=on'){
+    //         window.location.href = 'card_content.html?ss=on';
+    //     } else {
+    //         window.location.href = 'card_content.html';
+    //     }
+    // });
 
-    $('.prev-page-btn').click(function(e){
-        e.preventDefault();
-        if(window.location.search == '?ss=on'){
-            window.location.href = 'index.html?ss=on';
-        } else {
-            window.location.href = 'index.html';
-        }
-    });
+    // $('.prev-page-btn').click(function(e){
+    //     e.preventDefault();
+    //     if(window.location.search == '?ss=on'){
+    //         window.location.href = 'index.html?ss=on';
+    //     } else {
+    //         window.location.href = 'index.html';
+    //     }
+    // });
 
-    if(window.location.search == '?ss=on'){
-        $('.save-screenshot').addClass('show');
-    }
+    // if(window.location.search == '?ss=on'){
+    //     $('.save-screenshot').addClass('show');
+    // }
 
-    $('.save-screenshot').click(function(e){
-        e.preventDefault();
-        screenshot();
-    });
+    // $('.save-screenshot').click(function(e){
+    //     e.preventDefault();
+    //     screenshot();
+    // });
 
     // p5.js
     var p5Switch = 0;
@@ -36,16 +36,16 @@ $(document).ready(function(){
         enableP5();
     }
 
-    function screenshot(){
-        html2canvas(document.getElementById('card-screenshot')).then(function(canvas) {
-            document.body.appendChild(canvas);
-            var a = document.createElement('a');
-            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-            a.download = 'image.jpg';
-            a.click();
-            canvas.style.display='none';
-        });
-    }
+    // function screenshot(){
+    //     html2canvas(document.getElementById('card-screenshot')).then(function(canvas) {
+    //         document.body.appendChild(canvas);
+    //         var a = document.createElement('a');
+    //         a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+    //         a.download = 'image.jpg';
+    //         a.click();
+    //         canvas.style.display='none';
+    //     });
+    // }
 
     function enableP5(){
         $('.image-content img').hide();
